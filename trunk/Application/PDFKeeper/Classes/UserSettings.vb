1fcdac2fc801c9b9d00d75aa139a626df81291d2
RobertFrasca_cp <SND\RobertFrasca_cp>
2015-08-01 20:04:07 +0000

    Issue: 1663 http://pdfkeeper.codeplex.com/workitem/1663 Rewrote UserSettings class.

--- trunk/Application/PDFKeeper/Classes/UserSettings.vb
+++ trunk/Application/PDFKeeper/Classes/UserSettings.vb
@@ -20,169 +20,129 @@
 '*
 '******************************************************************************
 
-Public Class UserSettings
-	Dim xmlFile As String = Path.Combine(RootDataDir, "UserSettings.xml")
-	Shared m_OpenFileLastFolder As String
-	Shared m_SaveFileLastFolder As String
-	Shared m_LastUserName As String
-	Shared m_LastDataSource As String
-	Shared m_FormPositionTop As String
-	Shared m_FormPositionLeft As String
-	Shared m_FormPositionHeight As String
-	Shared m_FormPositionWidth As String
-	Shared m_FormPositionWindowState As String
-	Shared m_UpdateCheck As String
-						
-	#Region "Properties"
+''' <summary>
+''' PDFKeeper application user settings.
+''' </summary>
+Public NotInheritable Class UserSettings
+	Shared settingsFile As String = Path.Combine(RootDataDir, _
+		"UserSettings.xml")
+	Shared _OpenFileLastFolder As String
+	Shared _SaveFileLastFolder As String
+	Shared _FormPositionHeight As String
+	Shared _FormPositionWidth As String
 	
-	Shared Property OpenFileLastFolder() As String
+	' Database Connection form settings.
+	
+	Shared Property LastUserName As String = Nothing
+	Shared Property LastDataSource As String = Nothing
+	
+	' Main form settings.
+	
+	Shared Property FormPositionTop As String = "10"
+	Shared Property FormPositionLeft As String = "25"
+	
+	Shared ReadOnly Property FormPositionDefaultHeight As Integer
 		Get
-			If m_OpenFileLastFolder = Nothing Then
-				m_OpenFileLastFolder = _
-						 My.Computer.FileSystem.SpecialDirectories.MyDocuments
+			Return 714 
+		End Get
+	End Property
+	
+	Shared Property FormPositionHeight As String
+		Get
+			Return _FormPositionHeight
+		End Get
+		Set(ByVal Value As String)
+			_FormPositionHeight = Value
+		End Set
+	End Property
+	
+	Shared ReadOnly Property FormPositionDefaultWidth As Integer
+		Get
+			Return 714 
+		End Get
+	End Property
+	
+	Shared Property FormPositionWidth As String
+		Get
+			Return _FormPositionWidth
+		End Get
+		Set(ByVal Value As String)
+			_FormPositionWidth = Value
+		End Set
+	End Property
+	
+	Shared Property FormPositionWindowState As String = "0"
+	Shared Property UpdateCheck As String = "1"
+	
+	' Common Dialogs settings.
+	
+	Shared Property OpenFileLastFolder As String
+		Get
+			If _OpenFileLastFolder = Nothing Then
+				_OpenFileLastFolder = _
+					My.Computer.FileSystem.SpecialDirectories.MyDocuments
 			End If
-			Return m_OpenFileLastFolder
+			Return _OpenFileLastFolder
 		End Get
 		Set(ByVal Value As String)
-			m_OpenFileLastFolder = Value
+			_OpenFileLastFolder = Value
 		End Set
 	End Property
 	
-	Shared Property SaveFileLastFolder() As String
+	Shared Property SaveFileLastFolder As String
 		Get
-			If m_SaveFileLastFolder = Nothing Then
-				m_SaveFileLastFolder = _
-						 My.Computer.FileSystem.SpecialDirectories.MyDocuments
+			If _SaveFileLastFolder = Nothing Then
+				_SaveFileLastFolder = _
+					My.Computer.FileSystem.SpecialDirectories.MyDocuments
 			End If
-			Return m_SaveFileLastFolder
+			Return _SaveFileLastFolder
 		End Get
 		Set(ByVal Value As String)
-			m_SaveFileLastFolder = Value
+			_SaveFileLastFolder = Value
 		End Set
 	End Property
-	
-	Shared Property LastUserName() As String
-		Get
-			Return m_LastUserName
-		End Get
-		Set(ByVal Value As String)
-			m_LastUserName = Value
-		End Set
-	End Property
-	
-	Shared Property LastDataSource() As String
-		Get
-			Return m_LastDataSource
-		End Get
-		Set(ByVal Value As String)
-			m_LastDataSource = Value
-		End Set
-	End Property
-
-	Shared Property FormPositionTop() As String
-		Get
-			If m_FormPositionTop = Nothing Then
-				m_FormPositionTop = CStr(10)
-			End If
-			Return m_FormPositionTop
-		End Get
-		Set(ByVal Value As String)
-			m_FormPositionTop = Value
-		End Set
-	End Property
-	
-	Shared Property FormPositionLeft() As String
-		Get
-			If m_FormPositionLeft = Nothing Then
-				m_FormPositionLeft = CStr(25)
-			End If
-			Return m_FormPositionLeft
-		End Get
-		Set(ByVal Value As String)
-			m_FormPositionLeft = Value
-		End Set
-	End Property
-	
-	Shared Property FormPositionHeight() As String
-		Get
-			Return m_FormPositionHeight
-		End Get
-		Set(ByVal Value As String)
-			m_FormPositionHeight = Value
-		End Set
-	End Property
-	
-	Shared Property FormPositionWidth() As String
-		Get
-			Return m_FormPositionWidth
-		End Get
-		Set(ByVal Value As String)
-			m_FormPositionWidth = Value
-		End Set
-	End Property
-	
-	Shared Property FormPositionWindowState() As String
-		Get
-			If m_FormPositionWindowState = Nothing Then
-				m_FormPositionWindowState = CStr(0)
-			End If
-			Return m_FormPositionWindowState
-		End Get
-		Set(ByVal Value As String)
-			m_FormPositionWindowState = Value
-		End Set
-	End Property
-	
-	Shared Property UpdateCheck() As String
-		Get
-			If m_UpdateCheck = Nothing Then
-				m_UpdateCheck = CStr(1)
-			End If
-			Return m_UpdateCheck
-		End Get
-		Set(ByVal Value As String)
-			m_UpdateCheck = Value
-		End Set
-	End Property
-	
-	#End Region
 	
 	''' <summary>
-	''' This function will read application user settings from UserSettings.XML
-	''' into class properties.  It should get called when the application is
-	''' started.
+	''' Private constructor required for FxCop compliance (CA1053).
+	''' </summary>
+	Private Sub New()
+	End Sub
+	
+	''' <summary>
+	''' Read user settings from UserSettings.xml.  This needs to be performed
+	''' during application startup.
 	''' </summary>
 	''' <returns>0 = Success, 1 = Failed</returns>
-	Public Function Read() As Integer
-		If ImportLegacyProperties() = 1 Then
+	Public Shared Function Read As Integer
+		If ImportDeprecatedProperties = 1 Then
 			Return 1
 		End If
-		If ImportLegacyRegistryValues() = 1 Then
+		If ImportDeprecatedRegistryValues = 1 Then
 			Return 1
 		End If
-		If System.IO.File.Exists(xmlFile) Then
+		If System.IO.File.Exists(settingsFile) Then
 			Try
-				Dim xmlConfig As New XmlConfigSource(xmlFile)
-				m_LastUserName = xmlConfig.Configs("DatabaseConnectionForm") _
-									  	  	.Get("LastUserName")
-				m_LastDataSource = xmlConfig.Configs("DatabaseConnectionForm") _
-									  		.Get("LastDataSource")
-				m_FormPositionTop = xmlConfig.Configs("MainForm") _
-									  		.Get("FormPositionTop")
-				m_FormPositionLeft = xmlConfig.Configs("MainForm") _
-									  		.Get("FormPositionLeft")
-				m_FormPositionHeight = xmlConfig.Configs("MainForm") _
-									  		.Get("FormPositionHeight")
-				m_FormPositionWidth = xmlConfig.Configs("MainForm") _
-									  		.Get("FormPositionWidth")
-				m_FormPositionWindowState = xmlConfig.Configs("MainForm") _
-									  		.Get("FormPositionWindowState")
-				m_UpdateCheck = xmlConfig.Configs("MainForm") _
-											.Get("UpdateCheck")
-				m_OpenFileLastFolder = xmlConfig.Configs("CommonDialogs") _
-								   .Get("OpenFileLastFolder")
-				m_SaveFileLastFolder = xmlConfig.Configs("CommonDialogs") _
-								   .Get("SaveFileLastFolder")
+				Dim xmlConfig As New XmlConfigSource(settingsFile)
+				LastUserName = xmlConfig.Configs("DatabaseConnectionForm") _
+					.Get("LastUserName")					
+				LastDataSource = xmlConfig.Configs("DatabaseConnectionForm") _
+					.Get("LastDataSource")					
+				FormPositionTop = xmlConfig.Configs("MainForm") _
+					.Get("FormPositionTop")					
+				FormPositionLeft = xmlConfig.Configs("MainForm") _
+					.Get("FormPositionLeft")					
+				FormPositionHeight = xmlConfig.Configs("MainForm") _
+					.Get("FormPositionHeight")
+				FormPositionWidth = xmlConfig.Configs("MainForm") _
+					.Get("FormPositionWidth")
+				FormPositionWindowState = xmlConfig.Configs("MainForm") _
+					.Get("FormPositionWindowState")
+				UpdateCheck = xmlConfig.Configs("MainForm") _
+					.Get("UpdateCheck")
+				OpenFileLastFolder = xmlConfig.Configs("CommonDialogs") _
+					.Get("OpenFileLastFolder")
+				SaveFileLastFolder = xmlConfig.Configs("CommonDialogs") _
+					.Get("SaveFileLastFolder")
 			Catch ex As System.NullReferenceException
 			Catch ex As UnauthorizedAccessException
 				MessageBoxWrapper.ShowError(ex.Message)
@@ -196,39 +156,39 @@ Public Class UserSettings
 	End Function
 
 	''' <summary>
-	''' This subroutine will write the class properties to UserSettings.XML.
-	''' It should get called when the application is closing.
+	''' Save user settings to UserSettings.xml.  This needs to be performed
+	''' when the Main Form is closing.
 	''' </summary>
-	Public Sub Write
+	Public Shared Sub Save
 		Try
 			Dim xmlConfig As New XmlConfigSource
 			xmlConfig.AddConfig("DatabaseConnectionForm")
 			xmlConfig.Configs("DatabaseConnectionForm").Set("LastUserName", _
-														     m_LastUserName)
+														     LastUserName)
 			xmlConfig.Configs("DatabaseConnectionForm").Set("LastDataSource", _
-														 	 m_LastDataSource)
+														 	 LastDataSource)
 			xmlConfig.AddConfig("MainForm")
 			xmlConfig.Configs("MainForm").Set("FormPositionTop", _
-											   m_FormPositionTop)
+											   FormPositionTop)
 			xmlConfig.Configs("MainForm").Set("FormPositionLeft", _
-											   m_FormPositionLeft)
+											   FormPositionLeft)
 			xmlConfig.Configs("MainForm").Set("FormPositionHeight", _
-											   m_FormPositionHeight)
+											   FormPositionHeight)
 			xmlConfig.Configs("MainForm").Set("FormPositionWidth", _
-											   m_FormPositionWidth)
+											   FormPositionWidth)
 			xmlConfig.Configs("MainForm").Set("FormPositionWindowState", _
-											   m_FormPositionWindowState)
-			xmlConfig.Configs("MainForm").Set("UpdateCheck", m_UpdateCheck)
+											   FormPositionWindowState)
+			xmlConfig.Configs("MainForm").Set("UpdateCheck", UpdateCheck)
 			xmlConfig.AddConfig("CommonDialogs")
-			If Not m_OpenFileLastFolder = Nothing Then
+			If Not OpenFileLastFolder = Nothing Then
 				xmlConfig.Configs("CommonDialogs").Set("OpenFileLastFolder", _
-														m_OpenFileLastFolder)				
+														OpenFileLastFolder)				
 			End If
-			If Not m_SaveFileLastFolder = Nothing Then
+			If Not SaveFileLastFolder = Nothing Then
 				xmlConfig.Configs("CommonDialogs").Set("SaveFileLastFolder", _
-														m_SaveFileLastFolder)
+														SaveFileLastFolder)
 			End If
-			xmlConfig.Save(xmlFile)
+			xmlConfig.Save(settingsFile)
 		Catch ex As UnauthorizedAccessException
 			MessageBoxWrapper.ShowError(ex.Message)
 		Catch ex As IOException
@@ -237,11 +197,10 @@ Public Class UserSettings
 	End Sub
 	
 	''' <summary>
-	''' This function will import a PDFKeeper 1.0.0 or 1.1.0 properties file
-	''' into class properties, and then delete the file.
+	''' Import deprecated PDFKeeper 1.0.0 or 1.1.0 user settings.
 	''' </summary>
 	''' <returns>0 = Success, 1 = Failed</returns>
-	Private Shared Function ImportLegacyProperties() As Integer
+	Private Shared Function ImportDeprecatedProperties() As Integer
 		Dim legacyProperties As String = Path.Combine(AppDataDir, "pdfkeeper.properties")
 		If System.IO.File.Exists(legacyProperties) Then
 			Try
@@ -249,12 +208,8 @@ Public Class UserSettings
 									 	 FileMode.Open)
 					Using oStreamReader As New StreamReader(oFileStream, _
 															Encoding.ASCII)
-						Dim lastDataSource As String
-						Dim lastUserName As String
-						lastDataSource = oStreamReader.ReadLine
-						lastUserName = oStreamReader.ReadLine
-						m_LastDataSource = lastDataSource.Substring(15)
-						m_LastUserName = lastUserName.Substring(13)
+						LastUserName = oStreamReader.ReadLine.Substring(13)
+						LastDataSource = oStreamReader.ReadLine.Substring(15)
 					End Using
 					oFileStream.Close
 				End Using
@@ -268,11 +223,10 @@ Public Class UserSettings
 	End Function
 	
 	''' <summary>
-	''' This function will import the PDFKeeper 2.0.0 through 2.4.0 registry
-	''' key values into class properties, and then delete the registry key.
+	''' Import deprecated PDFKeeper 2.0.0 through 2.4.0 user settings.
 	''' </summary>
 	''' <returns>0 = Success, 1 = Failed</returns>
-	Private Shared Function ImportLegacyRegistryValues() As Integer
+	Private Shared Function ImportDeprecatedRegistryValues() As Integer
 		Dim appRoot As String = "HKEY_CURRENT_USER\Software\" & _
 								 Application.ProductName
 		Dim regKey As Microsoft.Win32.RegistryKey = _
@@ -280,23 +234,22 @@ Public Class UserSettings
 			  		 "Software\" & Application.ProductName, True)
 		If Not regKey Is Nothing Then
 			Try
-				m_LastUserName = CStr(Registry.GetValue(appRoot, _
-											"LastUserName", Nothing))
-				m_LastDataSource = CStr(Registry.GetValue(appRoot, _
-											"LastDataSource", Nothing))
-				m_FormPositionTop = CStr(Registry.GetValue(appRoot, _
-											"FormPositionTop", Nothing))
-				m_FormPositionLeft = CStr(Registry.GetValue(appRoot, _
-											"FormPositionLeft", Nothing))
-				m_FormPositionHeight = CStr(Registry.GetValue(appRoot, _
-											"FormPositionHeight", Nothing))
-				m_FormPositionWidth = CStr(Registry.GetValue(appRoot, _
-											"FormPositionWidth", Nothing))
-				m_FormPositionWindowState = CStr(Registry.GetValue(appRoot, _
-											"FormPositionWindowState", _
-											 Nothing))
-				m_UpdateCheck = CStr(Registry.GetValue(appRoot, _
-											"UpdateCheck", Nothing))
+				LastUserName = CStr(Registry.GetValue(appRoot, _
+					"LastUserName", Nothing))
+				LastDataSource = CStr(Registry.GetValue(appRoot, _
+					"LastDataSource", Nothing))
+				FormPositionTop = CStr(Registry.GetValue(appRoot, _
+					"FormPositionTop", Nothing))
+				FormPositionLeft = CStr(Registry.GetValue(appRoot, _
+					"FormPositionLeft", Nothing))
+				FormPositionHeight = CStr(Registry.GetValue(appRoot, _
+					"FormPositionHeight", Nothing))
+				FormPositionWidth = CStr(Registry.GetValue(appRoot, _
+					"FormPositionWidth", Nothing))
+				FormPositionWindowState = CStr(Registry.GetValue(appRoot, _
+					"FormPositionWindowState", Nothing))
+				UpdateCheck = CStr(Registry.GetValue(appRoot, _
+					"UpdateCheck", Nothing))
 				registry.CurrentUser.DeleteSubKey("Software\" & _
 					Application.ProductName)
 			Catch ex as IOException
