9127a3b06b2cf70776bd6280a3a56b3e25136b7f
Robert F. Frasca <rffrasca@gmail.com>
2019-10-21 18:09:17 -0400

    Changed "PDFKeeper has detected that you're upgrading from version 4.1.0 or earlier" to "PDFKeeper has detected that you've upgraded from an older version" in the UpgradeMessage resource.

--- src/PDFKeeper.WindowsApplication/My Project/Resources.Designer.vb
+++ src/PDFKeeper.WindowsApplication/My Project/Resources.Designer.vb
@@ -385,13 +385,13 @@ Namespace My.Resources
         End Property
         
         '''<summary>
-        '''  Looks up a localized string similar to PDFKeeper has detected that you&apos;re upgrading from version 4.1.0 or earlier.
+        '''  Looks up a localized string similar to PDFKeeper has detected that you&apos;ve upgraded from an older version.
         '''
         '''PDFKeeper 5.0.0 requires the following:
         '''
         '''* The database schema must be upgraded to the PDFKeeper 5.0.0 schema by an individual responsible for the administration of the Oracle Database.  Please see the &quot;Database Schema Upgrade for Oracle Database&quot; help topic.
         '''
-        '''* The Oracle Data Provider for .NET must be upgraded on this computer.  Please see the &quot;Installing Oracle Data Provider for .NET&quot; help topic located in &quot;Additional Clie [rest of string was truncated]&quot;;.
+        '''* The Oracle Data Provider for .NET must be upgraded on this computer.  Please see the &quot;Installing Oracle Data Provider for .NET&quot; help topic located in &quot;Additional Client Prereq [rest of string was truncated]&quot;;.
         '''</summary>
         Friend ReadOnly Property UpgradeMessage() As String
             Get
