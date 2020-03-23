94912963205eaec1e90013b56eef51b62dae3abc
Robert F. Frasca <rffrasca@gmail.com>
2020-03-22 20:18:21 -0400

    Targeting .NET Framework 4.8 when building application and made it a prerequisite in the installer.
    Signed-off-by: Robert F. Frasca <rffrasca@gmail.com>

--- src/PDFKeeper.WindowsApplication/My Project/Resources.Designer.vb
+++ src/PDFKeeper.WindowsApplication/My Project/Resources.Designer.vb
@@ -22,7 +22,7 @@ Namespace My.Resources
     '''<summary>
     '''  A strongly-typed resource class, for looking up localized strings, etc.
     '''</summary>
-    <Global.System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0"),  _
+    <Global.System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0"),  _
      Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
      Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
      Global.Microsoft.VisualBasic.HideModuleNameAttribute()>  _
@@ -416,7 +416,7 @@ Namespace My.Resources
                 Return ResourceManager.GetString("UploadConfig", resourceCulture)
             End Get
         End Property
-
+        
         '''<summary>
         '''  Looks up a localized string similar to Upload cycle cannot be started at this time because it is already executing or blocked from starting..
         '''</summary>
