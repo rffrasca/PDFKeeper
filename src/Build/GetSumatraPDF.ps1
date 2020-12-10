858d69d4b53c0cf61d4240dcb3cfad0b6d4b7623
Robert F. Frasca <rffrasca@gmail.com>
2020-12-09 21:01:53 -0500

    Created script that will download and extract SumatraPDF during the build process if missing or not the required version.
    Signed-off-by: Robert F. Frasca <rffrasca@gmail.com>

---
+++ src/Build/GetSumatraPDF.ps1
@@ -0,0 +1,38 @@
+﻿#******************************************************************************
+#* PDFKeeper -- Open Source PDF Document Management System
+#* Copyright (C) 2009-2021  Robert F. Frasca
+#*
+#* This file is part of PDFKeeper.
+#*
+#* PDFKeeper is free software: you can redistribute it and/or modify
+#* it under the terms of the GNU General Public License as published by
+#* the Free Software Foundation, either version 3 of the License, or
+#* (at your option) any later version.
+#*
+#* PDFKeeper is distributed in the hope that it will be useful,
+#* but WITHOUT ANY WARRANTY; without even the implied warranty of
+#* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#* GNU General Public License for more details.
+#*
+#* You should have received a copy of the GNU General Public License
+#* along with PDFKeeper.  If not, see <http://www.gnu.org/licenses/>.
+#******************************************************************************
+
+$folder = "..\..\..\..\vendor"
+$version = "3.1.2"
+$zipFileUrl = "https://www.sumatrapdfreader.org/dl/SumatraPDF-$version-64.zip"
+$zipFile = "$folder\SumatraPDF-$version-64.zip"
+$exeFile = "$folder\sumatraPDF.exe"
+
+if (Test-Path $exeFile) {
+    $file = Get-Item $exeFile
+    if ($file.VersionInfo.ProductVersion -eq $version) {
+        Write-Output "SumatraPDF $version was found in $folder"
+        exit 0
+    }
+}
+
+Invoke-WebRequest -Uri $zipFileUrl -OutFile $zipFile
+Expand-Archive -path $zipFile -DestinationPath $folder -Force
+
+exit 0
