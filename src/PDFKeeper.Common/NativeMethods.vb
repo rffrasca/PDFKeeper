ec8cd208dc1d4faadbc0454e6978380682becc63
Robert F. Frasca <rffrasca@gmail.com>
2022-08-07 13:34:23 -0400

    Downloads folder in the user profile is retrieved without using a third-party library.
    Signed-off-by: Robert F. Frasca <rffrasca@gmail.com>

--- src/PDFKeeper.Common/NativeMethods.vb
+++ src/PDFKeeper.Common/NativeMethods.vb
@@ -17,24 +17,19 @@
 '* You should have received a copy of the GNU General Public License
 '* along with PDFKeeper.  If not, see <http://www.gnu.org/licenses/>.
 '******************************************************************************
-Public NotInheritable Class UserProfileFolders
-    ''' <summary>
-    ''' Gets the Documents folder path.
-    ''' </summary>
-    ''' <returns>Folder path name</returns>
-    Public Shared ReadOnly Property Documents As String
-        Get
-            Return My.Computer.FileSystem.SpecialDirectories.MyDocuments
-        End Get
-    End Property
+Imports System.Runtime.InteropServices
 
+Public NotInheritable Class NativeMethods
     ''' <summary>
-    ''' Gets the Downloads folder path.
+    ''' Gets the full path of a known folder identified by the KNOWNFOLDERID of the folder.
     ''' </summary>
-    ''' <returns>Folder path name</returns>
-    Public Shared ReadOnly Property Downloads As String
-        Get
-            Return NativeMethods.SHGetKnownFolderPath(New Guid("374DE290-123F-4565-9164-39C4925E467B"), 0, Nothing)
-        End Get
-    End Property
+    ''' <param name="rfid">Reference to the KNOWNFOLDERID</param>
+    ''' <param name="dwFlags">Flags that specify special retrieval options. This value can be 0.</param>
+    ''' <param name="hToken">Access token that represents a particular user. Nothing represents the current user.</param>
+    ''' <returns></returns>
+    <DllImport("shell32.dll", CharSet:=CharSet.Unicode, ExactSpelling:=True, PreserveSig:=False)>
+    <DefaultDllImportSearchPaths(DllImportSearchPath.System32)>
+    Friend Shared Function SHGetKnownFolderPath(<MarshalAs(UnmanagedType.LPStruct)> ByVal rfid As Guid,
+                                                ByVal dwFlags As UInteger, ByVal hToken As IntPtr) As String
+    End Function
 End Class
