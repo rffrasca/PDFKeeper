3100fb9a878394be32a774cf23e2e9960c759441
Robert F. Frasca <rffrasca@gmail.com>
2023-12-25 11:20:30 -0500

    Set version to 9.1.1.
    Signed-off-by: Robert F. Frasca <rffrasca@gmail.com>

--- src/Build/SetVersion.ps1
+++ src/Build/SetVersion.ps1
@@ -18,8 +18,8 @@
 # * with PDFKeeper. If not, see <https://www.gnu.org/licenses/>.
 # *****************************************************************************
 
-$previousVersion="9.0.0"
-$currentVersion="9.1.0"
+$previousVersion="9.1.0"
+$currentVersion="9.1.1"
 
 (Get-ChildItem -Include *AssemblyInfo.*,PDFKeeper.Setup.wixproj,Product.wxs,'THIRD-PARTY-NOTICES.*' -Recurse ) |
 Foreach-Object {
