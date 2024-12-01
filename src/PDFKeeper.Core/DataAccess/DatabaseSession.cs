f5354933ce67de383f652c5df026aceb0963031f
Robert F. Frasca <rffrasca@gmail.com>
2024-12-01 11:25:09 -0500

    Restored and updated files that were corrupted following copyright year range update.
    Signed-off-by: Robert F. Frasca <rffrasca@gmail.com>

---
+++ src/PDFKeeper.Core/DataAccess/DatabaseSession.cs
@@ -0,0 +1,206 @@
+// ****************************************************************************
+// * PDFKeeper -- Open Source PDF Document Management
+// * Copyright (C) 2009-2025 Robert F. Frasca
+// *
+// * This file is part of PDFKeeper.
+// *
+// * PDFKeeper is free software: you can redistribute it and/or modify it
+// * under the terms of the GNU General Public License as published by the
+// * Free Software Foundation, either version 3 of the License, or (at your
+// * option) any later version.
+// *
+// * PDFKeeper is distributed in the hope that it will be useful, but WITHOUT
+// * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// * more details.
+// *
+// * You should have received a copy of the GNU General Public License along
+// * with PDFKeeper. If not, see <https://www.gnu.org/licenses/>.
+// ****************************************************************************
+
+using Microsoft.Win32;
+using PDFKeeper.Core.Application;
+using PDFKeeper.Core.DataAccess.Repository;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Security;
+
+namespace PDFKeeper.Core.DataAccess
+{
+    public static class DatabaseSession
+    {
+        private static CompatiblePlatformName platformName;
+        private static string userName;
+        private static SecureString password;
+        private static string dataSource;
+        private static string oracleWalletPath;
+        private static string localDatabasePath;
+        private static bool oracleOdpNetHandlerEnabled;
+
+        /// <summary>
+        /// Compatible database platform name.
+        /// </summary>
+        public enum CompatiblePlatformName
+        {
+            Sqlite, // 0
+            Oracle  // 1
+        }
+
+        /// <summary>
+        /// Gets or sets the database platform name.
+        /// </summary>
+        public static CompatiblePlatformName PlatformName
+        {
+            get => platformName;
+            set
+            {
+                platformName = value;
+                OnCompatiblePlatformNameChanged();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the database user name. On a get, the operating system user name will be
+        /// returned when the database platform is SQLite.
+        /// </summary>
+        public static string UserName
+        {
+            get
+            {
+                if (PlatformName.Equals(CompatiblePlatformName.Sqlite))
+                {
+                    return Environment.UserName;
+                }
+                else
+                {
+                    return userName;
+                }
+            }
+            set => userName = value;
+        }
+
+        /// <summary>
+        /// Gets or sets the database password SecureString. On a get, null will be returned when
+        /// the database platform is SQLite.
+        /// </summary>
+        public static SecureString Password
+        {
+            get
+            {
+                if (PlatformName.Equals(CompatiblePlatformName.Sqlite))
+                {
+                    return null;
+                }
+                else
+                {
+                    return password;
+                }
+            }
+            set
+            {
+                if (value != null)
+                {
+                    password = new SecureString();
+                    password = value;
+                    password.MakeReadOnly();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the data source. On a get, null will be returned when the database
+        /// platform is SQLite.
+        /// </summary>
+        public static string DataSource
+        {
+            get
+            {
+                if (PlatformName.Equals(CompatiblePlatformName.Sqlite))
+                {
+                    return null;
+                }
+                else
+                {
+                    return dataSource;
+                }
+            }
+            set => dataSource = value;
+        }
+
+        /// <summary>
+        /// Gets the Oracle Wallet path used for Mutual TLS (mTLS) authentication or null will be
+        /// returned when an Oracle Wallet is not setup.
+        /// </summary>
+        public static string OracleWalletPath
+        {
+            get
+            {
+                oracleWalletPath = (string)Registry.GetValue(
+                    ApplicationRegistry.UserKeyPath,
+                    "OracleWalletPath",
+                    null);
+                return oracleWalletPath;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the local SQLite database path.
+        /// </summary>
+        public static string LocalDatabasePath
+        {
+            get
+            {
+                localDatabasePath = Registry.GetValue(
+                    ApplicationRegistry.UserKeyPath,
+                    "LocalDatabasePath",
+                    new ApplicationDirectory().GetDirectory(
+                        ApplicationDirectory.SpecialName.ApplicationData)).ToString();
+                var filePath = new FileInfo(
+                    Path.Combine(localDatabasePath,
+                    string.Concat(
+                        new ExecutingAssembly().ProductName,
+                        ".sqlite")));
+                return filePath.FullName;
+            }
+            set => Registry.SetValue(
+                ApplicationRegistry.UserKeyPath,
+                "LocalDatabasePath",
+                Path.GetDirectoryName(value));
+        }
+
+        /// <summary>
+        /// Gets or sets if the documents list has changes? (true or false)
+        /// </summary>
+        public static bool DocumentsListHasChanges { get; set; }
+
+        /// <summary>
+        /// Gets a document repository instance for the database platform in use.
+        /// </summary>
+        /// <returns>The document repository instance.</returns>
+        public static IDocumentRepository GetDocumentRepository()
+        {
+            return DocumentRepositoryFactory.Create();
+        }
+
+        private static void OnCompatiblePlatformNameChanged()
+        {
+            if (PlatformName.Equals(CompatiblePlatformName.Oracle) && !oracleOdpNetHandlerEnabled)
+            {
+                AppDomain.CurrentDomain.AssemblyResolve += LoadOracleOdpNet;
+                oracleOdpNetHandlerEnabled = true;
+            }
+        }
+
+        private static Assembly LoadOracleOdpNet(object sender, ResolveEventArgs args)
+        {
+            var assemblyPath = Path.Combine(
+                (string)Registry.GetValue(
+                    ApplicationRegistry.UserKeyPath,
+                    "OracleOdpNetPath",
+                    string.Empty),
+                "Oracle.ManagedDataAccess.dll");
+            return Assembly.LoadFile(assemblyPath);
+        }
+    }
+}
---
+++ src/PDFKeeper.Core/FileIO/TextExtractor/ImageTextExtractor.cs
@@ -0,0 +1,89 @@
+// ****************************************************************************
+// * PDFKeeper -- Open Source PDF Document Management
+// * Copyright (C) 2009-2025 Robert F. Frasca
+// *
+// * This file is part of PDFKeeper.
+// *
+// * PDFKeeper is free software: you can redistribute it and/or modify it
+// * under the terms of the GNU General Public License as published by the
+// * Free Software Foundation, either version 3 of the License, or (at your
+// * option) any later version.
+// *
+// * PDFKeeper is distributed in the hope that it will be useful, but WITHOUT
+// * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// * more details.
+// *
+// * You should have received a copy of the GNU General Public License along
+// * with PDFKeeper. If not, see <https://www.gnu.org/licenses/>.
+// ****************************************************************************
+
+using PDFKeeper.Core.Application;
+using System;
+using System.Collections.ObjectModel;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
+using Windows.Media.Ocr;
+
+namespace PDFKeeper.Core.FileIO.TextExtractor
+{
+    internal class ImageTextExtractor
+    {
+        private readonly Collection<byte[]> images;
+        private readonly ImageFormat imageFormat;
+
+        /// <summary>
+        /// Initializes a new instance of the ImageTextExtractor class.
+        /// </summary>
+        /// <param name="images">The collection of images.</param>
+        /// <param name="imageFormat">The image format of the images.</param>
+        internal ImageTextExtractor(Collection<byte[]> images, ImageFormat imageFormat)
+        {
+            this.images = images;
+            this.imageFormat = imageFormat;
+        }
+
+        /// <summary>
+        /// Gets text from a collection of images using Windows OCR.
+        /// </summary>
+        /// <returns>The extracted text.</returns>
+        internal async Task<string> GetText()
+        {
+            var text = new StringBuilder();
+            foreach (var image in images)
+            {
+                try
+                {
+                    var imageFile = Path.Combine(new ApplicationDirectory().GetDirectory(
+                        ApplicationDirectory.SpecialName.Temp).FullName,
+                        String.Concat(Guid.NewGuid(), ".", imageFormat.ToString()));
+                    File.WriteAllBytes(imageFile, image);
+                    using (var stream = File.Open(imageFile, FileMode.Open, FileAccess.Read))
+                    {
+                        var bmpDecoder = await BitmapDecoder.CreateAsync(
+                            stream.AsRandomAccessStream()).AsTask().ConfigureAwait(false);
+                        using (var softwareBmp = await bmpDecoder.GetSoftwareBitmapAsync())
+                        {
+                            if (softwareBmp.PixelWidth <= OcrEngine.MaxImageDimension &&
+                                softwareBmp.PixelHeight <= OcrEngine.MaxImageDimension)
+                            {
+                                var ocrEngine = OcrEngine.TryCreateFromUserProfileLanguages();
+                                var ocrResult = await ocrEngine.RecognizeAsync(softwareBmp);
+                                foreach (var line in ocrResult.Lines)
+                                {
+                                    text.AppendLine(line.Text);
+                                }
+                            }                                
+                        }
+                    }
+                    File.Delete(imageFile);
+                }
+                catch (ArithmeticException) { }
+            }
+            return text.ToString();
+        }
+    }
+}
